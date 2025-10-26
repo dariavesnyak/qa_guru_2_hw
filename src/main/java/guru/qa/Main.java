5fd7dd17cf17891af4b5db9cd9d6472e667758ed
Daxator <149147260+dariavesnyak@users.noreply.github.com>
2025-10-26 22:48:24 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,7 +4,7 @@ package guru.qa;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static String msg = "Hello, qa.guru!!!";
+    private static String msg = "Hello, qa.guru2!!!!!!";
     
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
