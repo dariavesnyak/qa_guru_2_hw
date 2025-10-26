1e4e752bed6da4dabb3a142da39a70e943797f23
Daxator <dariavesnyak@gmail.com>
2025-10-26 22:29:10 +0300

    added method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -6,7 +6,11 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello, qa.guru!");
+        System.out.printf(getMessage());
 
+    }
+
+    private static String getMessage() {
+        return "Hello, qa.guru!";
     }
 }
