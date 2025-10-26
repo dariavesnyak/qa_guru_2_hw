11def393431f59a8a50406ec9989803e484a8a32
Daxator <149147260+dariavesnyak@users.noreply.github.com>
2025-10-26 22:27:27 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,10 +3,13 @@ package guru.qa;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static String msg = ""Hello, qa.guru!"";
+    
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello, qa.guru!");
+        System.out.printf(msg);
 
     }
 }
